f07f5724a4c3c36f1fbaeeeff63cdf2703f2539b
hsjoo126 <hsjoo126@naver.com>
2024-05-03 17:04:03 +0900

    로그인 기능 구현 완료/view없음, jwt로 처리

--- sparta_news/accounts/urls.py
+++ sparta_news/accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView
+)
 
 urlpatterns = [
     #회원가입
     path('',views.AccountListAPIView.as_view()),
+    #로그인
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
