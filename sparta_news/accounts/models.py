5c69a1564ef06d9d9c8179a910bb4321fb4165c1
JeongYoonGi <jygmyjesus@nate.com>
2024-05-07 10:32:39 +0900

    게시글 등록,수정,삭제,조회

--- sparta_news/accounts/models.py
+++ sparta_news/accounts/models.py
@@ -3,9 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-<<<<<<< HEAD
-    pass
-
-=======
     created_at = models.DateTimeField(auto_now_add=True)
->>>>>>> 9813829457e85f4ec2966711c2eeae49790eaf0a
+
