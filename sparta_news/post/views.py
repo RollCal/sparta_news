6872f1ab75f5df347e9ec719629347bc10bec3a2
JeongYoonGi <jygmyjesus@nate.com>
2024-05-07 16:36:15 +0900

    게시글 클래스 수정

--- sparta_news/post/views.py
+++ sparta_news/post/views.py
@@ -1,77 +1,20 @@
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PostSerializer
-from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
-from django.http import JsonResponse
 from .models import spartanews
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
-@csrf_exempt
-def sparta_news_list(request):
-    if request.method == 'GET':
-        news_list = spartanews.objects.all()
-        serializer = PostSerializer(news_list, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class SpartaNewsList(generics.ListCreateAPIView):
+    queryset = spartanews.objects.all()
+    serializer_class = PostSerializer
 
-# 게시글 생성
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-@csrf_exempt
-def sparta_news_create(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-# 게시글 상세 조회
-
-
-@csrf_exempt
-def sparta_news_detail(request, pk):
-    try:
-        news = spartanews.objects.get(pk=pk)
-    except spartanews.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = PostSerializer(news)
-        return JsonResponse(serializer.data)
-
-# 게시글 수정
-
-
-@csrf_exempt
-def sparta_news_update(request, pk):
-    try:
-        news = spartanews.objects.get(pk=pk)
-    except spartanews.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(news, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-# 게시글 삭제
-
-
-@csrf_exempt
-def sparta_news_delete(request, pk):
-    try:
-        news = spartanews.objects.get(pk=pk)
-    except spartanews.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'DELETE':
-        news.delete()
-        return HttpResponse(status=204)
+class SpartaNewsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = spartanews.objects.all()
+    serializer_class = PostSerializer
