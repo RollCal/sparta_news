6efbd5dbbcb8d29073e39e3be407f2d730cc9c5c
Jungho Lee <awjdgh1111@gmail.com>
2024-05-09 00:16:24 +0900

    embedding에러잡기 보류

--- sparta_news/search_engine/views.py
+++ sparta_news/search_engine/views.py
@@ -1,16 +1,10 @@
 from rest_framework import generics
-
 from .models import Document
 from post.models import spartanews
 from .serializers import DocumentSerializer
 from . import search
 from rest_framework.response import Response
 from django.db.models import Q
-
-
-class DocumentListView(generics.ListAPIView):
-    serializer_class = DocumentSerializer
-    queryset = Document.objects.all()
 
 
 class SearchDocumentView(generics.RetrieveAPIView):
@@ -22,15 +16,17 @@ class SearchDocumentView(generics.RetrieveAPIView):
         # search.py 파일에서 search 함수를 사용하여 검색 수행
         검색결과 = search.search(검색어)
 
-        # 검색 결과(content)가 포함된 SpartanNews 객체 필터링
-        # 부분 문자열을 포함하는 경우를 처리하기 위해 Q 객체를 사용합니다.
-        해당_문서 = spartanews.objects.filter(
-            Q(content__contains=검색어) |
-            Q(title__contains=검색어)
-        )
+        # 검색 결과가 실제 검색어와 관련이 있는지 확인하여 필터링
+        관련있는_검색결과 = [item for item in 검색결과 if 검색어 in item.content or 검색어 in item.title]
+# 리스트, 튜플 대조 -> 검색어와 유사도
+        # 검색 결과를 SpartanNews 객체의 ID로 변환
+        관련있는_검색결과_ids = [item.id for item in 관련있는_검색결과]
 
-        # 필터링된 문서 직렬화
-        직렬화된_데이터 = self.get_serializer(해당_문서, many=True).data
+        # 검색 결과(ID) 기반으로 SpartanNews 객체 필터링
+        해당_문서들 = spartanews.objects.filter(id__in=관련있는_검색결과_ids)
+
+        # 검색된 문서만 직렬화
+        직렬화된_데이터 = self.serializer_class(해당_문서들, many=True).data
 
         return Response({
             'results': 직렬화된_데이터
