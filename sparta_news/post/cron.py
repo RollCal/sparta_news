992e8f8453af2267196d96cca348c094e5581a61
hsjoo126 <hsjoo126@naver.com>
2024-05-09 14:27:18 +0900

    포인트 차감시 차감된 시각 알 수 있도록 기능 추가

--- sparta_news/post/cron.py
+++ sparta_news/post/cron.py
@@ -1,7 +1,9 @@
 from .models import spartanews
+from datetime import datetime
 
 def update_news_point():
-    print('포인트가 차감되었습니다')
+    now = datetime.now()
+    print('포인트가 차감되었습니다. 차감된 시각:', now)
     posts = spartanews.objects.all()
     for post in posts:
         post.point -= 5 
